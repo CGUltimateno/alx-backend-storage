9ab630cf110ee493e9ef6d88fd14ed1c548c0518
CGU <Mohameddeaf65@gmail.com>
2024-03-21 10:03:34 +0200

    1

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -4,10 +4,10 @@
 -- name VARCHAR(255)
 -- email VARCHAR(255)
 -- country enum('USA', 'CAN', 'MEX')
-CREATE TABLE users (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255),
-  email VARCHAR(255) NOT NULL UNIQUE,
-   country CHAR(2) NOT NULL DEFAULT 'US',
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country CHAR(2) NOT NULL DEFAULT 'US',
 	CHECK (country = 'US' OR country = 'CO' OR country = 'TN')
 );
