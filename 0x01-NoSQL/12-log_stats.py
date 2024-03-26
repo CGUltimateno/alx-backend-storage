e00a6e08bef6fceecba2d3966e2862f5c9c11eb3
CGU <Mohameddeaf65@gmail.com>
2024-03-26 04:58:11 +0200

    tasks

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,1 +1,28 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+a Python function that provides some
+ stats about Nginx logs stored in MongoDB
+"""
+from pymongo import MongoClient
+
+
+def log_stats(mongo_collection, q_key=None, q_val=None):
+    """
+    a Python function that provides some
+    stats about Nginx logs stored in MongoDB
+    """
+    return mongo_collection.count_documents({} if q_key is None and q_val is None else {q_key: q_val})
+
+
+if __name__ == "__main__":
+    client = MongoClient("mongodb://localhost:27017")
+    logs = client.logs.nginx
+    print(log_stats(logs))
+    print(log_stats(logs, 'path', '/status'))
+    print(log_stats(logs, 'method', 'GET'))
+    print(log_stats(logs, 'method', 'POST'))
+    print(log_stats(logs, 'method', 'PUT'))
+    print(log_stats(logs, 'method', 'PATCH'))
+    print(log_stats(logs, 'method', 'DELETE'))
+
+
--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,1 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+a Python function that inserts a new
+document in a collection based on kwargs
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    a Python function that inserts a new
+    document in a collection based on kwargs
+    """
+    return mongo_collection.insert_one(kwargs).inserted_id
