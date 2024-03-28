22fa7e33f9f4d4544789f2737ef0258336618a40
CGUltimateno <Mohameddeaf65@gmail.com>
2024-03-28 17:51:50 +0200

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -62,27 +62,21 @@ class Cache:
 
     @call_history
     @count_calls
-    def store(self, data: Union[str, int, float]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store the input data in Redis and return a unique key"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Callable = None):
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
         """Get the data stored in Redis for a given key"""
-        if fn is int:
-            return self.get_int(self._redis.get(key))
-        elif fn is str:
-            return self.get_str(self._redis.get(key))
-        elif fn is None:
-            return self._redis.get(key)
-        else:
-            return fn(self._redis.get(key))
+        data = self._redis.get(key)
+        return fn(data) if fn is not None else data
 
     def get_str(self, key: str) -> str:
         """Get the data stored in Redis for a given key as a string"""
-        return key.decode("utf-8")
+        return self.get(key, lambda x: x.decode('utf-8'))
 
     def get_int(self, key: str) -> int:
         """Get the data stored in Redis for a given key as an integer"""
-        return int(key.decode("utf-8"))
+        return self.get(key, lambda x: int(x))
